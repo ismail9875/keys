953c27a08fd9360c0fb4df2f3506b0224034d211
Ismail Saidi <83674911+ismail9875@users.noreply.github.com>
2022-12-10 11:54:50 +0100

    some change

--- installer.sh
+++ installer.sh
@@ -1,22 +1,8 @@
 echo "##############################################"
 echo "############ KEYADDER[Bissfeed] ##############"
 echo "##############################################"
-
-#Cheking OS
-if [ -f /etc/apt/apt.conf ] ; then
-    STATUS='/var/lib/dpkg/status'
-    OS='DreamOS'
-elif [ -f /etc/opkg/opkg.conf ] ; then
-   STATUS='/var/lib/opkg/status'
-   OS='Opensource'
-fi
-if [$OS='DreamOS'] ; then
-    wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.deb "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1_all.deb?raw=true"
-    dpkg install --force-overwrite /tmp/*.deb
-    else
-        wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1all.ipk"
-        opkg install --force-overwrite /tmp/*.ipk
-fi
+wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1all.ipk"
+opkg install --force-overwrite /tmp/*.ipk
 echo ""
 cd ..
 sync
