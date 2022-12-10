d87d0352db104f1143e43800363b961dcd9a1685
Ismail Saidi <83674911+ismail9875@users.noreply.github.com>
2022-12-10 11:56:33 +0100

    Create installer1.sh

---
+++ installer1.sh
@@ -0,0 +1,16 @@
+echo "##############################################"
+echo "############ KEYADDER[Bissfeed] ##############"
+echo "##############################################"
+wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1all.deb"
+dpkg install --force-overwrite /tmp/*.deb
+echo ""
+cd ..
+sync
+echo "##############################################"
+echo "############ Installation Successful  ########"
+echo "############   Restart Enigma2 GUI... ########" 
+echo "##############################################"
+init 4
+sleep 2
+init 3
+exit 0
