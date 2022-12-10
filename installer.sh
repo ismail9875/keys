dc2096091ec50584410cfad459af4a40e1dcbdb2
Ismail Saidi <83674911+ismail9875@users.noreply.github.com>
2022-12-10 11:31:37 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,16 +2,10 @@ echo "##############################################"
 echo "############ KEYADDER[Bissfeed] ##############"
 echo "##############################################"
 
-#Cheking OS (if OS is opensource will be install ipk file, else deb file will be install)
+#Cheking OS (Downloading File)
 
-if [ $OS = "Opensource"]; then
-    wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1_all.ipk?raw=true"
-    opkg install --force-overwrite /tmp/*.ipk
-else
-    wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.deb "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1_all.deb?raw=true"
-
-    dpkg install --force-overwrite /tmp/*.deb
-fi
+wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1_all.ipk?raw=true"
+opkg install --force-overwrite /tmp/*.ipk
 echo ""
 cd ..
 sync
