5d6d81f125d5969fc31711743f7b4892b22b5ee7
Ismail Saidi <83674911+ismail9875@users.noreply.github.com>
2022-12-11 13:52:05 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 echo "##############################################"
 echo "############| KEYADDER[Bissfeed] |############"
 echo "##############################################"
-wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1all.ipk?raw=true"
+wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1_all.ipk?raw=true"
 opkg install --force-overwrite /tmp/*.ipk
 echo ""
 cd ..
