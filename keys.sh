4baf27c16b764c9978dd03444333654457492f37
ismail_e2_os <83674911+ismail9875@users.noreply.github.com>
2023-12-24 14:20:08 +0100

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -13,7 +13,11 @@ echo "Please enter Service Keys : ";read serviceKey;
 echo "Please entre the name of channel : " ; read serviceName
 echo "F ${serviceId}${VPid} 00000000 ${serviceKey} ; ${serviceName}" | tee a- >> /etc/tuxbox/config/SoftCam.Key
 echo "key has been add to SoftCam.Key"
-rm /tmp/keys.sh
+if [ -f /home/root/keys.sh  ] ; then
+rm /home/root/*.sh
+else
+rm /tmp/*.sh
+fi
 echo "================================="
 echo "== Thanks for using our script =="
 echo "================================="
