0cf3e6020392a36c466b991298f37bfa4bd7067a
ismail_e2_os <83674911+ismail9875@users.noreply.github.com>
2023-12-31 18:55:40 +0100

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -11,7 +11,9 @@ echo "Please enter Service id : ";read serviceId;
 echo "Please enter Service VideoPid : "; read VPid
 echo "Please enter Service Keys : ";read serviceKey;
 echo "Please entre the name of channel : " ; read serviceName
-echo "F ${serviceId}${VPid} 00000000 ${serviceKey} ; ${serviceName}" | tee a- >> /etc/tuxbox/config/SoftCam.Key
+echo "
+F ${serviceId}${VPid} 00000000 ${serviceKey} ; ${serviceName}
+"| tee a- >> /etc/tuxbox/config/SoftCam.Key
 echo "key has been add to SoftCam.Key"
 if [ -f /home/root/keys.sh  ] ; then
 rm /home/root/*.sh
